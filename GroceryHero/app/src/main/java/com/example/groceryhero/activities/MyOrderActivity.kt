4f080761ffefeafe192ca5e55e14300a2906b515
Seth <sethosrictorralba@gmail.com>
2020-05-22 19:16:04 -0500

    Added my order layout

---
+++ GroceryHero/app/src/main/java/com/example/groceryhero/activities/MyOrderActivity.kt
@@ -0,0 +1,12 @@
+package com.example.groceryhero.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyOrderActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_order)
+    }
+}
